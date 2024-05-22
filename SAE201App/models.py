1b674b01fd31d9e53e47b7389c727166a8654cba
Julien Mauroy <pro.julien.mauroy@gmail.com>
2024-05-22 12:42:57 +0200

    fix: correction indentation

--- SAE201App/models.py
+++ SAE201App/models.py
@@ -4,15 +4,13 @@ from django.db import models
 from django.db import models
 
 class Auteurs(models.Model):
-    nom = models.CharField(blank =False, max_length=100)
+    nom = models.CharField(blank=False, max_length=100)
     prenom = models.CharField(blank=False, max_length=100)
     age = models.IntegerField(blank=False)
-    photo= models.ImageField(null = True)
-def __str__(self):
-    chaine = f"{self.nom} {self.prenom} {self.age} {self.photo}"
+    photo = models.ImageField(null=True)
 
-# Create your models here.
-
+    def __str__(self):
+        return f"{self.nom} {self.prenom} {self.age}"
 
 class CommentaireJeux(models.Model):
     jeux: models.ForeignKey["models.Model"] = models.ForeignKey(
