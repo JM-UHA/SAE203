8938075f903d8e86f04eb1641dbeb9d7fefdc9ff
Julien Mauroy <pro.julien.mauroy@gmail.com>
2024-05-22 12:36:20 +0200

    feat: modèle commentaire de jeux, ajout de Pillow en dépendance

--- SAE201App/models.py
+++ SAE201App/models.py
@@ -1,3 +1,20 @@
+import typing
+
 from django.db import models
 
 # Create your models here.
+
+
+class CommentaireJeux(models.Model):
+    jeux: models.ForeignKey["models.Model"] = models.ForeignKey(
+        "Jeux", on_delete=models.CASCADE
+    )  # todo: modifier signature
+    joueurs: models.ForeignKey["models.Model"] = models.ForeignKey(
+        "Joueurs", on_delete=models.SET_NULL
+    )  # todo: modifier signature
+    note = models.IntegerField()
+    commentaire = models.TextField(max_length=5000)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+MODELS: typing.List[typing.Type[models.Model]] = [CommentaireJeux]
