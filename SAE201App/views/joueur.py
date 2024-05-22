da74ef6a403b2103399f64fc9cfaa148afe5c8b0
theophile <theophile.bouquet@uha.fr>
2024-05-22 15:51:35 +0200

    creation dossier joueur

--- SAE201App/views/joueur.py
+++ SAE201App/views/joueur.py
@@ -1,2 +1,15 @@
 from django.shortcuts import render
 from ..forms import JoueurForm
+
+def ajouter_joueur(request):
+    """Sert à ajouter un joueur"""
+    if request.method == "Envoyer":
+        form = JoueurForm(request)
+        if form.is_valid():
+            Joueur = form.save()
+            return render(request,"joueur/view.html",{"joueur" : Joueur})
+        else:
+            return render(request,"joueur/create.html",{"form": form})
+    else:
+        form = JoueurForm()
+        return render(request,"joueur/create.html",{"form":form})
