c8b7ac20db06dfc1181e7a28b4b9a4456a3c66d4
Lydia <julien.mauroy@uha.fr>
2024-05-22 18:49:00 +0200

    feat: Commentaires + autres fichiers divers (#3)
    /
    
    ---------
    
    Co-authored-by: Theophile Bouquet <theophile.bouquet@uha.fr>

--- SAE201App/forms.py
+++ SAE201App/forms.py
@@ -5,7 +5,7 @@ from . import models
 
 class AuteursForm(ModelForm):
     class Meta:
-        model = models.Auteurs
+        model = models.Auteur
         fields = ("nom", "prenom", "age", "photo")
         labels = {
             "nom": "Nom",
@@ -19,3 +19,21 @@ class JoueurForm(ModelForm):
     class Meta:
         model = models.Joueur
         fields = ("nom", "prenom", "mail", "type")
+        labels = {
+            "nom": "Nom",
+            "prenom": "Prénom",
+            "mail": "Mail",
+            "type": "Type",
+        }
+
+
+class CommentaireForm(ModelForm):
+    class Meta:
+        model = models.CommentaireJeu
+        fields = ("jeu", "joueur", "note", "commentaire")
+        labels = {
+            "jeu": "Jeu",
+            "joueur": "Joueur",
+            "note": "Votre note sur 10",
+            "commentaire": "Commentaire",
+        }
