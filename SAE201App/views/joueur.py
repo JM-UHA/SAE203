3156247e1995a4f3b3dc508f4389add7b264645d
theophile <theophile.bouquet@uha.fr>
2024-05-22 16:56:34 +0200

    creation HTML joueur

--- SAE201App/views/joueur.py
+++ SAE201App/views/joueur.py
@@ -13,3 +13,8 @@ def ajouter_joueur(request):
     else:
         form = JoueurForm()
         return render(request,"joueur/create.html",{"form":form})
+
+def read_joueur(request,id):
+    """permet de voir une fiche joueur"""
+    joueur = models.joueur.object.get(pk=id)
+    return render(request, "joueur/view.html", {"Joueur": joueur})
